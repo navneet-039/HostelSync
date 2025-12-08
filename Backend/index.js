a7d4eaaeebfbdf82f3196a9c5aad4a1ffacf8b78
Navneet Kumar <navneet.889035@gmail.com>
2025-12-08 20:42:53 +0530

    .env file

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = 8000;
+
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Server is running ...");
 });
+const PORT = process.env.PORT||8001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,55 +1,60 @@
-const mongoose=require("mongoose");
-const UserSchema=new mongoose.Schema({
-  name:{
-    type:String,
-    required:true
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    registrationNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Student", "Supervisor", "Chief_warden"],
+      required: true,
+    },
+    //for students
+    roomNumber: {
+      type: String,
+      default: null,
+    },
+    hostel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hostel",
+      default: null,
+    },
+    branch: {
+      type: String,
+      default: null,
+    },
+    year: {
+      type: String,
+      default: null,
+    },
+    supervisorOfHostel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hostel",
+      default: null,
+    },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
   },
-  email:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  phone:{
-    type:String,
-    required:true
-  },
-  registrationNumber:{
-    type:String,
-    required:true
-  },
-  password:{
-    type:String,
-    required:true
-  },
-  role:{
-    type:String,
-    enum:["Student","Supervisor","Chief_warden"],
-    required:true
-  },
-  //for students
-  roomNumber:{
-    type:String,
-    default:null
-  },
-  hostel:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Hostel",
-    default:null
-  },
-  branch:{
-    type:String,
-    default:null
-  },
-  year:{
-    type:String,
-    default:null
-  },
-  supervisorOfHostel:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Hostel",
-    default:null
-  },
-
-
-},{timestamps:true});
-module.exports=mongoose.model("User",UserSchema);
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", UserSchema);
