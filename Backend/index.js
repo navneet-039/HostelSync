6ad7bd83e93a1bca4b29db62a8296de942b9b295
Navneet Kumar <navneet.889035@gmail.com>
2025-12-13 19:47:05 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,20 +3,25 @@ const express = require("express");
 const database = require("./Config/mongodb");
 const app = express();
 const userRoutes = require("./routes/User");
-const complaintRoutes = require("../routes/complaint");
-const complaintQueryRoutes = require("./routes/supervisor");
+const complaintRoutes = require("./routes/complaint");
+const supervisorRoutes = require("./routes/supervisor");
 
 
 
 
 require("dotenv").config();
 
-
+database.connect();
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running ...");
 });
+
+app.use("/api/users", userRoutes);
+app.use("/api/complaints", complaintRoutes);
+app.use("/api/supervisor", supervisorRoutes);
 const PORT = process.env.PORT || 8001;
-database.connect();
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
