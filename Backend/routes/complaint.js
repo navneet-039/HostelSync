05c91ca681b34466de487e389ce9275f49be61ad
garimasingh116 <garimasingh0316@gmail.com>
2026-01-03 17:38:44 +0530

    me

--- Backend/routes/complaint.js
+++ Backend/routes/complaint.js
@@ -2,8 +2,18 @@ import express from "express";
 import { auth,isSupervisor,isStudent } from "../middlewares/Auth.js";
 import { registerComplaint, updateComplaintStatus } from "../controllers/Complaint.js";
 import { getAllStudent } from "../controllers/Supervisor.js";
+import upload from "../middlewares/multer.js";
 
 const router = express.Router();
+ 
+
+router.post(
+  "/register",
+  auth,
+  isStudent,
+  upload.array("images", 5),
+  registerComplaint
+);
 
 router.post("/register", auth,isStudent, registerComplaint);
 router.patch("/status/:complaintId", auth,isSupervisor, updateComplaintStatus,getAllStudent);
