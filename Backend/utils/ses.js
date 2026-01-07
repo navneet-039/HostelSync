7f0cea54e0e9e860b25aa2444486c9dd5f4a6529
Navneet Kumar <navneet.889035@gmail.com>
2026-01-07 22:13:44 +0530

    ses.js

---
+++ Backend/utils/ses.js
@@ -0,0 +1,29 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+const ses = new SESClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+export const sendComplaintMail = async ({ to, subject, html }) => {
+  await ses.send(
+    new SendEmailCommand({
+      source: process.env.SES_FROM_MAIL,
+      Destination: {
+        ToAddresses: [to],
+      },
+      Message: {
+        Subject: {
+          Data: subject,
+        },
+        Body: {
+          Html: {
+            Data: html,
+          },
+        },
+      },
+    })
+  );
+};
